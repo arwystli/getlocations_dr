7e627debdb17f2cb74986379a6fae5af00c7d63d
hutch <hutch@midwales.net>
2013-03-28 11:42:41 +0000

    better pathing for ajax calls

--- js/getlocations.js
+++ js/getlocations.js
@@ -167,6 +167,7 @@ var getlocations_settings = {};
         var fullscreen = (settings.fullscreen ? true : false);
         var js_path = settings.js_path;
 
+        global_settings.info_path = settings.info_path;
         global_settings.preload_data = settings.preload_data;
         if (settings.preload_data) {
           global_settings.getlocations_info = Drupal.settings.getlocations_info[key];
@@ -690,7 +691,7 @@ var getlocations_settings = {};
                 }
               }
               else {
-                var path = Drupal.settings.basePath + gs.locale_prefix + "getlocations_cb/info";
+                var path = gs.info_path;
                 var qs = {'lid': lid, 'key': lidkey};
                 if (gs.show_distance) {
                   if ($("#getlocations_search_slat_" + mkey).is('div')) {
