042d38fc3d2a3a5950ee5948ff1d2c26e4bbe099
hutch <hutch@midwales.net>
2013-06-11 09:36:17 +0100

    added search_places feature

--- modules/getlocations_search/js/getlocations_search_admin.js
+++ modules/getlocations_search/js/getlocations_search_admin.js
@@ -240,6 +240,21 @@
         }
       });
 
+      // search_places
+      if ($("#edit-getlocations-search-defaults-search-places").attr('checked')) {
+        $("#wrap-getlocations-search-places").show();
+      }
+      else {
+        $("#wrap-getlocations-search-places").hide();
+      }
+      $("#edit-getlocations-search-defaults-search-places").change(function() {
+        if ($(this).attr('checked')) {
+          $("#wrap-getlocations-search-places").show();
+        }
+        else {
+          $("#wrap-getlocations-search-places").hide();
+        }
+      });
 
       // search marker
       if ($("#edit-getlocations-search-defaults-do-search-marker").is('input')) {
